42605e30097a80833c62cad9ae4e81d34aff6d10
libin <civet148@126.com>
2020-11-09 17:40:33 +0800

    [apple-pay] use apple pay verify url inner constants

--- example/main.go
+++ example/main.go
@@ -5,14 +5,9 @@ import (
 	"github.com/civet148/gotools/log"
 )
 
-const (
-	APPLE_PAY_VERIFY_URL_PROD    = "https://buy.itunes.apple.com/verifyReceipt"     //production env
-	APPLE_PAY_VERIFY_URL_SANDBOX = "https://sandbox.itunes.apple.com/verifyReceipt" //sandbox env
-)
-
 func main() {
 
-	pay := applepay.NewApplePay("A0sd1Fw9df0", APPLE_PAY_VERIFY_URL_PROD)
+	pay := applepay.NewApplePay("A0sd1Fw9df0", applepay.APPLE_PAY_VERIFY_URL_PROD)
 	if _, err := pay.VerifyReceipt("MIIbWQYJKoZIhvcNAQcCoIIbSjCCG0YCAQExCzAJBgUrDgMCGgUA=="); err != nil {
 		log.Errorf("apple pay receipt verify error [%s]", err)
 	} else {
